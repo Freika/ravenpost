30ad422635b8ffb283b65a074a668722701496a3
Freika <iamfrey@gmail.com>
2017-04-26 18:21:05 +0700

    Add basic actions and views for messages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: 'messages#index'
 
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :messages
 end
