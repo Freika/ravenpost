944568dedd80ca5ed8ca9639f144ecaf8c559aed
Freika <iamfrey@gmail.com>
2017-04-27 19:27:54 +0700

    Add basic tests for recipients and non-recipient.

--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -1,9 +1,30 @@
 module FeatureHelpers
+  include Warden::Test::Helpers
+
   def select2(value, **options)
     script = <<-JS
       $('select##{options[:from]}').siblings('.select2-container').find('.select2-selection').trigger('click')
     JS
     page.execute_script(script)
     find(".select2-results__option", text: value).click
+  end
+
+  def self.included(base)
+    base.before(:each) { Warden.test_mode! }
+    base.after(:each) { Warden.test_reset! }
+  end
+
+  def sign_in(resource)
+    login_as(resource, scope: warden_scope(resource))
+  end
+
+  def sign_out(resource)
+    logout(warden_scope(resource))
+  end
+
+  private
+
+  def warden_scope(resource)
+    resource.class.name.underscore.to_sym
   end
 end
