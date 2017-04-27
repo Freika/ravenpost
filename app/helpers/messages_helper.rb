5ca907959fb8b3372a0ce6b0d02f991f531a0206
Freika <iamfrey@gmail.com>
2017-04-27 21:44:26 +0700

    Indicate unread messages visually.

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -6,4 +6,10 @@ module MessagesHelper
   def page_title
     params[:controller].gsub('messages/', '').capitalize
   end
+
+  def read_class(message)
+    if message.user_messages.find_by(user: current_user).fresh?
+      'list-group-item-info'
+    end
+  end
 end
