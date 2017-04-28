57e0b493f8e72ee204074dda69faed3996bbc315
Freika <iamfrey@gmail.com>
2017-04-28 23:21:00 +0700

    Move filtering to concern.

--- app/controllers/messages/inbox_controller.rb
+++ app/controllers/messages/inbox_controller.rb
@@ -4,20 +4,10 @@ class Messages::InboxController < ApplicationController
   private
 
   def set_messages
-    messages = current_user.received_messages
+    @messages = current_user.received_messages
                   .includes(:sender)
                   .includes(:user_messages)
                   .paginate(page: params[:page])
                   .order(created_at: :desc)
-
-    if params[:sender].present?
-      messages = messages.where(sender_id: params[:sender])
-    end
-
-    if params[:query].present?
-      messages = messages.search_full_text(params[:query])
-    end
-
-    @messages = messages
   end
 end
